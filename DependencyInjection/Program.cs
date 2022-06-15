c3e62ceba2e05e33b4a09bd073a06e0b0a6676db
Adithya Vijay K <adithyavj@gmail.com>
2022-06-15 13:56:31 +0530

    Add Serilog for logging

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,6 +1,7 @@
 using DependencyInjection.Logic;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddTransient<IDemoLogic, BetterDemoLogic>(); // when asked for IDemoLogic, give DemoLogic
+
+builder.Host.UseSerilog((context, config) =>
+{
+    config.WriteTo.Console();
+});
 
 var app = builder.Build();
 
